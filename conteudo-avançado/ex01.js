ff263f3512c40157caceabe532dd55c64285bcad
Ericka Vanessa <vanessaerikagomes54@gmail.com>
2024-04-18 17:23:42 -0300

    html-js

---
+++ conteudo-avançado/ex01.js
@@ -0,0 +1,7 @@
+let val = 30;
+if (val > 60.5) {// condições simples 
+  console.log(`Você ultrapassou a velocidade permitida. Multado!`);
+}
+
+console.log(`A velocidade do seu carro e ${val}km/h`);
+console.log(`Dirija sempre usando cinto de segurança`);
